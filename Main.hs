3b6ac8d6f776f131743040033cc36910126566aa
Lukas Toth <lukas@lukastoth.com>
2013-12-18 13:03:48 -0500

    Refactor Main.hs to support set arguments

--- Main.hs
+++ Main.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
-module Main where
+module Main
+( main
+, filterCards
+, getSet
+, getCards
+) where
 
 import Control.Applicative
 import Data.Aeson
@@ -9,17 +14,17 @@ import Data.Maybe (fromMaybe)
 
 import MagicCards
 
-getCards :: IO (Maybe [Card])
-getCards = getSet >>= return . (cards <$>)
+getCards :: FilePath -> IO (Maybe [Card])
+getCards fp = getSet fp >>= return . (cards <$>)
 
 debugSet :: IO (Either String CardSet)
 debugSet =  eitherDecode <$> L.readFile setFile
 
-getSet :: IO (Maybe CardSet)
-getSet =  decode <$> L.readFile setFile
+getSet :: FilePath -> IO (Maybe CardSet)
+getSet fp =  decode <$> L.readFile fp
 
 filterCards :: (Card -> Bool) -> IO [Card]
-filterCards p = getCards >>= return . (filter p) . (fromMaybe [])
+filterCards p = getCards setFile >>= return . (filter p) . (fromMaybe [])
 
 p1 = (\c -> rarity c == MythicRare && cmc c == Just 5)
 p2 = (\c -> R `elem` fromMaybe [] (manaCost c))
